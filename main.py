00138c21310c587197708807a557985391749ba9
Daniquinteero <113439174+Daniquinteero@users.noreply.github.com>
2022-09-13 15:52:57 +0100

    my function
    we wanted the function to return the result added

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def my(a, b):
-    return a-b
+    return a+b
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm ok')
